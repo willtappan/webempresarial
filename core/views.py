1c6139c0cb29033fb231454648f784a99052eab0
William Tappan <ing.tappan@gmail.com>
2020-03-12 12:02:31 -0600

    primer commit del proyecto webempresa con Django

---
+++ core/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render , HttpResponse
+
+# Create your views here.
+'''
+Inici Home
+Historia ABout
+Servicios services 
+Visitanos store
+Contact
+Blog 
+Sambple
+
+'''
+
+'''
+def home(request):
+    return HttpResponse("Inicio")
+
+def about(request):
+    return HttpResponse("Historia")
+
+def services(request):
+    return HttpResponse("Servicios")
+    
+def store(request):
+    return HttpResponse("visitanos")
+def contact(request):
+    return HttpResponse("Contacto")
+def blog(request):
+    return HttpResponse("Blog")
+def sample(request):
+    return HttpResponse("Sambple")
+'''
+
+def home(request):
+    return render(request, "core/home.html")
+def about(request):
+    return render(request, "core/about.html")
+def services(request):
+    return render(request, "core/services.html")
+def store(request):
+    return render(request, "core/store.html")
+def contact(request):
+    return render(request, "core/contact.html")
+def blog(request):
+    return render(request, "core/blog.html")
+def sample(request):
+    return render(request, "core/sample.html")
