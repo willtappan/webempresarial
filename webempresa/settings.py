ba22703dc63c66772f5971eff0e77ce78db1349d
William Tappan <ing.tappan@gmail.com>
2020-03-18 12:46:40 -0600

    Class 44: Quinta APP [social]

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'core',
-    'services.apps.ServicesConfig'
+    'services.apps.ServicesConfig',
+    'social.apps.SocialConfig',
 
 ]
 
